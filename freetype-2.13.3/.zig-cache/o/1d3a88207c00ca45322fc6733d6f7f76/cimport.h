63c75672213ddbee8e0b9903cf5a779be75c722f
aidan <aidanaden@hotmail.com>
2025-05-12 16:00:22 +0800

    chore: single gitignore

--- freetype-2.13.3/.zig-cache/o/1d3a88207c00ca45322fc6733d6f7f76/cimport.h
+++
@@ -1,9 +0,0 @@
-#include <freetype/ftadvanc.h>
-#include <freetype/ftbbox.h>
-#include <freetype/ftbitmap.h>
-#include <freetype/ftcolor.h>
-#include <freetype/ftlcdfil.h>
-#include <freetype/ftsizes.h>
-#include <freetype/ftstroke.h>
-#include <freetype/fttrigon.h>
-#include <freetype/ftsynth.h>
